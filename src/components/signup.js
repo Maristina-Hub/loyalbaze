2220040a4189b2706effd72da6971f314c0980f5
Maristina-Hub <maristyna@yahoo.com>
2023-02-11 01:19:27 +0100

    loyal baze task

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,46 +4,51 @@ const SignUp = () => {
   return (
     <div className="body">
       <div className="card">
-        <div className="border flex justify-between mx-[153px] pt-10">
-          <div className="border border-red">
-            <h1 className=" flex">
-              I
-              <span>
+        <div className=" flex justify-between mx-[60px] pt-6">
+          <div className=" w-[231px] h-[62px] m-[4px] p-[10px]" >
+            <h1 className="flex flex-row h-[62px]  m-[4px]">
+              <span className="text-[48px]"> I </span>
+              <span className="top-[12.93%] bottom-[12.91%] left-[8.33%x] right-[8.33%] m-[4px] ">
                 <img src={require("../img/Vector.svg").default} alt="" />
               </span>
-              yalbaze
+              <span className="w-[178px] text-[48px]">yalbaze</span>
             </h1>
           </div>
-          <div className="  m-2">
-            <button className="btn border border-blue border-x-pink-500 border-t-pink-500">
-              Get Priority Access
-            </button>
+          <div className="m-2  ">
+            <button className="btn2">Get Priority Access</button>
           </div>
         </div>
-        <div>
-          <h1 className="w-[802px] h-[166px]">Turn your best customers into Loyal fans</h1>
-          <p>
+        <div className="m-auto mt-[12px] mx-[64px] pt-8">
+          <h1 className="text-center m-auto  mt-[12px] text-[64px] h-[166px] w-[802px] leading-[83.2px]">
+            Turn your best customers into{" "}
+            <span className="text-[#A75FD2]">Loyal fans</span>
+          </h1>
+          <p className="m-auto text-center text-[24px] h-[131px] w-[938px] leading-[130%]">
             Loyalbaze, empowers businesses to offer digital, mobile-first
             loyalty programs to their customers. With our AI Powered platform,
             you can easily create and manage custom loyalty and rewards
             programs, track customer engagement, and gain valuable insights to
             improve your business. Sign up now to get early access.
           </p>
-          <input
-            type="text"
-            name="name"
-            placeholder="Tell us your name "
-            className="input"
-          />
-          <input
-            type="email"
-            name="name"
-            placeholder="Enter your email address "
-            className="input"
-          />
-          <button>Get early access</button>
+          <div className="  mt-[10px] flex flex-col px-[14px] py[12px] gap[8px] ml-[380px] text-center">
+            <input
+              type="text"
+              name="name"
+              placeholder="Tell us your name "
+              className="input"
+            />
+            <input
+              type="email"
+              name="name"
+              placeholder="Enter your email address "
+              className="input"
+            />
+            <button className="btn">Get early access</button>
 
-          <p> +57 Joined</p>
+            <div className="mt-[8px]  mr-[380px]">
+              <p> +57 Joined</p>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -109,8 +114,8 @@ const SignUp = () => {
           </div>
         </div>
         <div className="footer">
-        <hr className="hr "/>
-        <h1 className="mt-12">hello world</h1>
+          <hr className="hr " />
+          <h1 className="mt-12">hello world</h1>
         </div>
       </div>
     </div>
